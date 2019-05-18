48d2125b2cec7af00d8e01bc36e772df54624687
Abdo Babukr <abdo.babukr@gmail.com>
2019-05-17 18:01:41 -0700

    Completed Blocks Up to QPSK Modulation

---
+++ MatlabScripts/Simulation1.m
@@ -0,0 +1,18 @@
+%% Read Text Message
+readID = fopen('message.txt');
+txtScan = textscan(readID,'%760c');
+tA = txtScan{1};
+
+%% Convert to Binary
+tBits = dec2bin(tA,8)';           
+tBits = reshape(tBits,numel(tBits),1);
+
+%% QPSK Encoding
+
+%% Convert from Binary
+rBits = reshape(tBits,8,numel(tBits)/8);
+
+%% Write Back Text Message
+rA = char(bin2dec(rBits')');
+writeID = fopen('received.txt','w');
+fprintf(writeID, rA);
