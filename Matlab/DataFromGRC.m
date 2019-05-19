c914b2e088d5e2ed21252719e03953191b278390
Abdo Babukr <abdo.babukr@gmail.com>
2019-05-19 01:10:18 -0700

    Passing Bytes to GRC

--- Matlab/DataFromGRC.m
+++ Matlab/DataFromGRC.m
@@ -1,6 +1,14 @@
-function [v] = DataFromGRC(filename,len)
+function [v] = DataFromGRC(filename,len,type)
     filename = strcat('../GRC/',filename,'.dat');
-    f = fopen(filename, 'rb'); 
-    v = fread(f,len,'float');
+    f = fopen(filename, 'rb');
+    switch(type)
+        case 'uint8'
+            v = fread(f,len,'uint8');
+        case 'complex'
+            r = fread(f,len,'float');
+            v = r(1:2:end) + j*r(2:2:end);
+    end
+    
     v = v';
-fclose(f);
+    fclose(f);
+end
--- Matlab/DataToGRC.m
+++ Matlab/DataToGRC.m
@@ -1,15 +1,16 @@
 function [] = DataToGRC(filename, data, type)
     % Writing a data file so it can be read from Gnu Radio
+    filename = strcat('../GRC/', filename, '.dat');
+    f = fopen (filename, 'wb'); 
     switch(type)
         case 'uint8'
             data = uint8(data);
+            fwrite (f, data , 'unsigned char');
         case 'single'
             data = single(data);
-    end
-    
-    filename = strcat('../GRC/', filename, '.dat');
-    f = fopen (filename, 'wb'); 
-    v = fwrite (f, data , 'unsigned char'); %specify float 
+            fwrite (f, data , 'float');
+    end  
     fclose (f);
+    
 end
     
