dd88de5c1270537409ae9b082e3da73fb18f3111
Abdo Babukr <abdo.babukr@gmail.com>
2019-05-23 03:07:52 -0700

    Cleaning Up Build Directory, disabling sink blocks

--- GRC/gr-qpsk/build/lib/test_qpsk_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-export VOLK_GENERIC=1
-export GR_DONT_LOAD_PREFS=1
-export srcdir=/home/foci2/Documents/SDR/QPSK/GRC/gr-qpsk/lib
-export PATH=/home/foci2/Documents/SDR/QPSK/GRC/gr-qpsk/build/lib:$PATH
-export LD_LIBRARY_PATH=/home/foci2/Documents/SDR/QPSK/GRC/gr-qpsk/build/lib:$LD_LIBRARY_PATH
-export PYTHONPATH=$PYTHONPATH
-test-qpsk 
--- GRC/gr-qpsk/build/python/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/foci2/Documents/SDR/QPSK/GRC/gr-qpsk/python
-# Build directory: /home/foci2/Documents/SDR/QPSK/GRC/gr-qpsk/build/python
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
